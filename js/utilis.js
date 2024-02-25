a257b2991cc4d208649e53d8f9102e864e069d4f
samandarwebcreator <samandarwebcreator@gmail.com>
2024-02-25 15:27:43 +0500

    done the partfolio

---
+++ js/utilis.js
@@ -0,0 +1,36 @@
+function get(item) {
+    if(document.querySelector(item)){
+        return document.querySelector(item)
+    } else{
+        return (`${item} is not found!`)
+    }
+}
+
+const parfolioArray = [
+    {
+        id: 1,
+        image: "../assets/images/Component1.jpg"
+    },
+    {
+        id: 2,
+        image: "../assets/images/Component2.jpg"
+    },
+    {
+        id: 3,
+        image: "../assets/images/Component3.jpg"
+    },
+    {
+        id: 4,
+        image: "../assets/images/Component4.jpg"
+    },
+    {
+        id: 5,
+        image: "../assets/images/Component5.jpg"
+    },
+    {
+        id: 6,
+        image: "../assets/images/Component6.jpg"
+    }
+]
+
+export {get, parfolioArray}
